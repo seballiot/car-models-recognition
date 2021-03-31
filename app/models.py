bf398776240ce1eb3eceae8bf12bb74ceefab9f7
seballiot <seballiot2b@gmail.com>
2021-03-31 23:27:41 +0200

    Database

--- app/models.py
+++ app/models.py
@@ -6,11 +6,8 @@ class Cars(db.Model):
 
     id = db.Column(db.String(120), primary_key=True)
     prix = db.Column(db.Integer())
-    capacit_de_reservoir = db.Column(db.Integer())
     Emission_de_CO2 = db.Column(db.Integer())
-    Nombre_de_porte = db.Column(db.Integer())
     Puissance = db.Column(db.Integer())
     consommation_urbaine = db.Column(db.Float())
     consommation_autoroute = db.Column(db.Float())
-    categorie = db.Column(db.String(120))
     vitesse_max = db.Column(db.Integer())
