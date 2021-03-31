2fa93693c958211b86c586a9f7aa498555106f45
seballiot <seballiot2b@gmail.com>
2021-03-31 15:09:00 +0200

    Mise à jour structure table

--- app/models.py
+++ app/models.py
@@ -10,3 +10,7 @@ class Cars(db.Model):
     Emission_de_CO2 = db.Column(db.Integer())
     Nombre_de_porte = db.Column(db.Integer())
     Puissance = db.Column(db.Integer())
+    consommation_urbaine = db.Column(db.Float())
+    consommation_autoroute = db.Column(db.Float())
+    categorie = db.Column(db.String(120))
+    vitesse_max = db.Column(db.Integer())
